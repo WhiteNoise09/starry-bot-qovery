7264537a5c63705f0137bab402fe8dd074c2f1b4
WhiteNoise09 <whitenoise09@free.fr>
2021-01-27 19:17:56 +0100

    log -> french time

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 module.exports = {
 	log(message) {
 		const date = new Date(Date.now());
-		console.log(`${date.toLocaleDateString().green} - ${date.toLocaleTimeString().cyan} : ${message}`);
+		console.log(`${date.toLocaleDateString('fr - FR').green} - ${date.toLocaleTimeString('fr - FR').cyan} : ${message}`);
 	}
 }
