4a92f6bd6ea4d23318cfb0bcd20499035edaa3ed
WhiteNoise09 <whitenoise09@free.fr>
2021-01-27 23:30:09 +0100

    reuse previous index.js

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('heyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy');
+// requires
+const Discord 	   = require('discord.js');
+const fs 	  	   = require('fs');
+const colors  	   = require('colors');
+const utils   	   = require('./utils');
+const config  	   = require('./config.json');
+
+console.log('hey');
+
+// client discord
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// namespace global
+global.SB = {
+	client: client,
+	config: config,
+	...utils
+}
+
+// récupération des events
+fs.readdirSync(config.events_path).forEach(file => {
+	const event = require(`${config.events_path}/${file}`);
+
+	event.once
+	? client.once(event.type, event.callback)
+	: client.on(event.type, event.callback);
+
+	SB.log(`registered '${event.type.yellow}' event : ${file.toString().gray}`);
+});
+
+// récupération des commandes
+fs.readdirSync(config.commands_path).forEach(file => {
+	const command = require(`${config.commands_path}/${file}`);
+
+	client.commands.set(command.name, command);
+
+	SB.log(`registered '${command.name.magenta} command : ${file.toString().gray}'`);
+});
+
+client.login(process.env.TOKEN); // login avec token, privé
+
+console.log('hey2');
