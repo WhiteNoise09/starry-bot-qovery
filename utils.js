14b880a9d9f6f1660768a964a449c8ba66e902c7
WhiteNoise09 <whitenoise09@free.fr>
2021-01-27 19:31:20 +0100

    changed IETF tag for locale date string : 'fr - FR' -> 'fr-FR'

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 module.exports = {
 	log(message) {
 		const date = new Date(Date.now());
-		console.log(`${date.toLocaleDateString('fr - FR').green} - ${date.toLocaleTimeString('fr - FR').cyan} : ${message}`);
+		console.log(`${date.toLocaleDateString('fr-FR').green} - ${date.toLocaleTimeString('fr-FR').cyan} : ${message}`);
 	}
 }
