339bdf8a3f5ae50ff2d877a007fa26e3a481e6b4
WhiteNoise09 <whitenoise09@free.fr>
2021-01-30 16:30:12 +0100

    j'ai mis des trucs pour afficher des messages pour savoir si il reçoit les messages

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'ping',
 	description: 'Pong !',
 	execute(message, args) {
+		console.log(`Hey from ${__filename} !`);
 		message.channel.send('pong !');
 	}
 }
--- events/commands.js
+++ events/commands.js
@@ -2,8 +2,10 @@ module.exports = {
 	type: 'message',
 	once: true,
 	callback(message) {
+		console.log(`Hey from ${__filename} !`);
 		const { prefix } = SB.config;
 		if(!message.content.startsWith(prefix)) return;
+		console.log(`Passed first condition in  ${__filename} !`);
 
 		const args = message.content.slice(prefix.length).trim().split(/ +/);
 		const commandName = args.shift().toLowerCase();
